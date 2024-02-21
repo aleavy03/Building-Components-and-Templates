06bca7428a19428c734d96cb18e22fe505e1a34c
aleavy03 <142447733+aleavy03@users.noreply.github.com>
2024-02-21 14:00:39 -0600

    Create practice-module.module.ts

---
+++ practice-module.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PracticeComponent} from "../../../Practice Components";
+
+@NgModule({
+  declarations: [PracticeComponent],
+
+  imports: [
+    CommonModule
+  ]
+})
+export class PracticeModule { }
+
