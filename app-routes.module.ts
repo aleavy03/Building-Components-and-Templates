7219adaec3d1344e543d3e3cc9246bd0a93975fc
aleavy03 <142447733+aleavy03@users.noreply.github.com>
2024-02-21 13:52:15 -0600

    Create app-routes.module.ts

---
+++ app-routes.module.ts
@@ -0,0 +1,14 @@
+// app-routing.module.ts
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  // Define your routes here
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
